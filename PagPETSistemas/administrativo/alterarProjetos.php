f84904d7b90bb6e38412dad8d90ba393e25de828
thiagoyf <thiagoyudifukunaga@gmail.com>
2012-08-22 21:39:55 -0400

    modified:   alterarProjetos.php 	modified:   cadastrarProjetos.php 	modified:   style.css

--- PagPETSistemas/administrativo/alterarProjetos.php
+++ PagPETSistemas/administrativo/alterarProjetos.php
@@ -1,10 +1,70 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+include("../includes/funcoes/conexao.php");
 ?>
 
+<script type="text/javascript" src="../includes/jQuery/jquery.js"> </script>
+<script type="text/javascript" src="../includes/jQuery/jquery.mask.js"> </script>
+<script type="text/javascript" >
+    $(document).ready(function() {
+        $("#dadosProjeto").hide("fast");
+    });
+    
+    $(document).ready(function() {
+        $("#mostrarDados").click(function() {
+            $("#dadosProjeto").show("fast");
+        });
+    });
+</script>
 
-<h2 align="center"> Alterar Projetos </h2>
+
+<?php
+$in = $_GET{"in"};
+
+if ($in == 1)
+    echo "<script type='text/javascript'> alert('Projeto Cadastrado com sucesso!')</script>";
+else if ($in == 2)
+    echo "<script type='text/javascript'> alert('Projeto não Cadastrado!')</script>";
+?>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#date").focus(function(){
+            $("#date").mask("99/99/9999");
+        });
+    });
+</script>
+
+<div>
+    <h2 align="center">Alterar Projetos</h2> 
+    <div>
+        <form name="input" action="atualizarProjeto.php" method="post">
+            <?php
+            $query1 = "select * from projeto";
+            $query1 = mysql_query($query1, $conexao);
+            ?>
+            
+            <b>Projetos:  </b><select  name="id_projeto" style="width:300px">
+                <?php while ($dados = mysql_fetch_array($query1)) { ?>
+                    <option value="<?php echo $dados[5] ?>"><?php echo $dados[0] ?></option>
+                <?php } ?>
+            </select>
+            <input id ="mostrarDados" type="button" value="Pesquisar"/>
+            
+            <?php
+            $query2 = "select * from projeto where projeto.id_projeto =  ";
+            $query2 = mysql_query($query2, $conexao);
+            
+				if(isset($_POST['id_projeto'])){
+						$id_projeto = $_POST['id_projeto'];
+				}            
+            ?>
+            
+            <br />
+            
+            <div id="dadosProjeto">
+            
+            </div>            
+            
+         </form>
+    </div>
+</div>
--- PagPETSistemas/administrativo/cadastrarProjetos.php
+++ PagPETSistemas/administrativo/cadastrarProjetos.php
@@ -1,3 +1,7 @@
+<?php
+include("../includes/funcoes/conexao.php");
+?>
+
 <script type="text/javascript" src="../includes/jQuery/jquery.js"> </script>
 <script type="text/javascript" src="../includes/jQuery/jquery.mask.js"> </script>
 
@@ -26,48 +30,63 @@ else if ($in == 2)
     });
 </script>
 
-<div id="main">
-    <h2 align="center">Página Para Cadastro de Projetos</h2> 
-    <div id="cadastroProjetos">
+<div>
+    <h2 align="center">Cadastro de Projetos</h2> 
+    <div>
         <form name="input" action="incluirProjeto.php" method="post">
-            <b>Nome Projeto:</b><input size="60px" type="text" name="nomeProjeto" />
-            <br/><br/>
+            <b>Nome do Projeto:</b>
+            <input size="60px" type="text" name="nomeProjeto" />
+            <br />
 
-            <b>Data de Inicio: </b><input id="datef" maxlength="10" size="8px" type="text" name="dataInicio" />
-            <br/><br/>
+            <b>Data de Inicio: </b>
+            <input id="datef" maxlength="10" size="8px" type="text" name="dataInicio" />
+            <br />
 
-            <b>Data de Fim: </b><input id="date" maxlength="10" size="8px" type="text" name="dataFim" />
-            <br/><br/>
-            <b>Descrição:</b><br/>
-            <textarea rows="10" cols="30" name ="desc"></textarea>
-            <br/><br/>
+            <b>Data de Fim: </b>
+            <input id="date" maxlength="10" size="8px" type="text" name="dataFim" />
+            <br />
+            
+            <b>Descrição:</b>
+            <br />
+            <textarea rows="10" cols="30" name ="desc" style="width:300px;height:100px"></textarea>
+            <br />
+            
             <?php
-            $conexao = mysql_connect("localhost", "root", "root") or die("falhou ao acessar o bd");
-            mysql_select_db("petsistemas", $conexao) or die("falhou ao acessar a tabela");
-            $query = "SELECT * FROM membros WHERE tipo = '1'";
+            $query = "select * from usuario as u join tutor as t on u.id_usuario = t.id_tutor";
             $query = mysql_query($query, $conexao);
             ?>
+            
             <b>Tutor:  </b><select  name="tutor">
                 <?php while ($dados = mysql_fetch_array($query)) { ?>
-                    <option value="<?php echo $dados[1] ?>"><?php echo $dados[0] ?></option>
+                    <option value="<?php echo $dados[3] ?>"><?php echo $dados[0] ?></option>
                 <?php } ?>
             </select>
-            <br/><br/>
+            <br />
+            
             <b>Tipo:  </b><select name="tipo">
                 <option value="1">ensino</option>
                 <option value="2">pesquisa</option>
                 <option value="3">extensão</option>
-                <option value="4">todos</option>
-            </select><br/><br/>
+            </select>
+            <br />
+            
             <?php
-            $query = "SELECT * FROM membros WHERE tipo = '2'";
+            $query = "SELECT * FROM usuario";
             $query = mysql_query($query, $conexao);
             ?>
-            <b>Alunos:  </b><br/>
+            
+            <b>Alunos:  </b>
+            <br />
+            
+            <select multiple="multiple" name="usuario_id_usuario" style="width:300px;height:200px">
             <?php while ($dados = mysql_fetch_array($query)) { ?>
-                <input type="checkbox" name="aluno[]" value="<?php echo $dados[1] ?>" /><?php echo $dados[0] ?><br />
-            <?php } ?><br/>
+                <option value="<?php echo $dados[0] ?>"><?php echo $dados[3] ?></option><br />
+            <?php } ?>
+            </select>
+            
+            <br />
             <input type="submit" value="Gravar"/>
+		   </form>
     </div>
 </div>
 

483c7c592942fd2d5e5c44bcd406bd3c19026e8a
BRUNO DE FREITAS MONTEIRO <brunogok@hotmail.com>
2012-08-23 20:19:50 -0400

    modified:   PagPETSistemas/noticias.php 	modified:   PagPETSistemas/projetos.php

--- PagPETSistemas/noticias.php
+++ PagPETSistemas/noticias.php
@@ -1,26 +1,40 @@
-<h1>Workshop de Engenharia de Software do MS</h1>
-<h6> 23/08/2012<h6>
-<div>
-<p>	Praesent nec risus ac sem suscipit faucibus. Donec vehicula sodales vestibulum. Quisque tristique, nunc non adipiscing convallis, leo dolor pulvinar enim,
- in eleifend est quam quis sapien. Fusce aliquet felis aliquam diam tempus in interdum augue aliquam. Mauris quis lorem elit, non lacinia eros. Aliquam porta 
- elementum faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-
-<p>	Quisque ligula eros, molestie rhoncus ullamcorper sit amet, rutrum eu ligula. Aenean facilisis, purus non adipiscing tempor, urna mi dictum ipsum, 
-eu dapibus nulla justo non odio. Aenean sagittis posuere quam, quis interdum lectus viverra non. Mauris consequat purus porttitor odio fermentum in 
-tristique urna egestas. Vestibulum pulvinar tristique leo, ut viverra nunc mollis a. Nulla ultrices mi ut dolor ultricies malesuada. Mauris at ante ut 
-ante auctor tincidunt vestibulum ac odio. Sed aliquam volutpat erat sit amet sodales. In et orci nunc, tincidunt mollis risus. Pellentesque mauris orci, 
-mollis vel fringilla non, tristique sit amet purus. Nullam blandit mauris quis justo varius pretium. Etiam elit libero, accumsan sed placerat ut, placerat 
-sed arcu. Fusce dictum, diam nec cursus condimentum, metus sem pulvinar turpis, ut placerat orci tortor et turpis. Mauris porta ante vitae elit faucibus 
-tincidunt. Duis et mauris in diam pharetra porttitor vel id quam. Curabitur ut tincidunt erat.</p>
-
-<p>	Nunc accumsan nisl vitae massa tincidunt sed consectetur ipsum auctor. Pellentesque mollis placerat sapien. Praesent ac ante sed mauris congue vulputate.
- Suspendisse enim tellus, ullamcorper nec pretium et, vulputate a ligula. Donec ac tellus nec lacus feugiat blandit. Morbi ut ante quam. Etiam in nulla eu 
- neque dictum bibendum at sit amet arcu.</p>
-
-<p>	Pellentesque consequat malesuada vehicula. Nunc non dignissim lorem. Vivamus nunc lacus, convallis et placerat ut, consequat a tortor. Phasellus convallis 
-tristique faucibus. Phasellus mollis laoreet hendrerit. Aliquam ligula est, facilisis porttitor fermentum eu, faucibus ut lacus. Donec tellus purus, ultricies 
-vel euismod in, pellentesque in lorem. Proin quis odio nec neque consectetur laoreet quis non elit. Quisque ut libero quis mauris congue accumsan eu sit 
-amet leo. Morbi aliquet ultricies sem ac pellentesque. Sed imperdiet velit in sapien cursus posuere. Phasellus vitae nulla odio. Donec elit lorem, gravida
-vel fringilla scelerisque, viverra at metus. In eu nisl purus. Nullam in quam in turpis ullamcorper pellentesque et ac ante. Mauris ullamcorper varius 
-erat, vel vestibulum neque rutrum quis</p>
+<h1>Notícias</h1>
+<div style =	"border-top: 1px solid gray;
+				border-bottom: 1px solid gray;
+				margin-bottom: 50px;
+				height: 40px;
+				background-color: #F8F8F8;">
+				
+<h2>Últimas notícias<h2>
 </div>
+
+<?php
+$numproj = 6;
+$count = 1;
+
+while($numproj > 0 && $count <= 10 && $count <= $numproj) {
+?>
+
+<a href="index.php"><img src="images/interrogacao.jpg" alt="interrogacao" align="left" width="76px" height="76px"></a>
+
+<div style="margin-left:86px">
+<p style =	"font-size:11px;
+			line-height: 5%;">
+			<b>Lorem</b></p>
+			
+<h2><a href="index.php" style =	"font-size: 20px;
+								color: black;">
+								Lorem ipsum</a></h2>
+								
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+</div>
+<br />
+<hr>
+
+
+<?php
+	$count += 1;
+}
+
+
+?>
--- PagPETSistemas/projetos.php
+++ PagPETSistemas/projetos.php
@@ -1,20 +1,40 @@
 <h1>Projetos</h1>
-<div id="div1">
+<div style =	"border-top: 1px solid gray;
+				border-bottom: 1px solid gray;
+				margin-bottom: 50px;
+				height: 40px;
+				background-color: #F8F8F8;">
+				
 <h2>Últimos projetos<h2>
 </div>
 
 <?php
 $numproj = 6;
 $count = 1;
+
 while($numproj > 0 && $count <= 10 && $count <= $numproj) {
-	echo '<a href="index.php"><img src="images/interrogacao.jpg" alt="interrogacao" align="left" width="76px" height="76px"></a>';
-	echo '<div style="margin-left:86px">';
-	echo '<p class="p1"><b>Lorem</b></p>';
-	echo '<h2><a href="index.php" class="link">Lorem ipsum</a></h2>';
-	echo '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>';
-	echo '</div>';
-	echo '<br />';
-	echo '<hr>';
+?>
+
+<a href="index.php"><img src="images/interrogacao.jpg" alt="interrogacao" align="left" width="76px" height="76px"></a>
+
+<div style="margin-left:86px">
+<p style =	"font-size:11px;
+			line-height: 5%;">
+			<b>Lorem</b></p>
+			
+<h2><a href="index.php" style =	"font-size: 20px;
+								color: black;">
+								Lorem ipsum</a></h2>
+								
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+</div>
+<br />
+<hr>
+
+
+<?php
 	$count += 1;
 }
+
+
 ?>

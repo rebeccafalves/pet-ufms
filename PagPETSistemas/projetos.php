8ce526776752e2ac6f45288ce03bf56a48b16e34
thiagoyf <thiagoyudifukunaga@gmail.com>
2012-09-03 16:31:29 -0400

    modified:   PagPETSistemas/projetos.php 	modified:   PagPETSistemas/style.css

--- PagPETSistemas/projetos.php
+++ PagPETSistemas/projetos.php
@@ -1,20 +1,23 @@
-<h1>Projetos</h1>
-<div id="div1">
-<h2>Últimos projetos<h2>
-</div>
+<h2 id="h2_custom1">Últimos projetos<h2>
 
 <?php
 $numproj = 6;
 $count = 1;
-while($numproj > 0 && $count <= 10 && $count <= $numproj) {
-	echo '<a href="index.php"><img src="images/interrogacao.jpg" alt="interrogacao" align="left" width="76px" height="76px"></a>';
-	echo '<div style="margin-left:86px">';
-	echo '<p class="p1"><b>Lorem</b></p>';
-	echo '<h2><a href="index.php" class="link">Lorem ipsum</a></h2>';
-	echo '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>';
-	echo '</div>';
-	echo '<br />';
-	echo '<hr>';
+
+echo '<div>' . "\n";
+echo '<p id="p_custom1"><b>Lorem</b></p>' . "\n";
+echo '<h2 id="h2_custom2"><a href="index.php" class="link">Lorem ipsum</a></h2>' . "\n";
+echo '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>' . "\n";
+echo '</div>' . "\n"; 
+echo "\n";
+
+while($numproj > 0 && $count <= 10 && $count < $numproj) {
+	echo '<div id="div_custom1">' . "\n";
+	echo '<p id="p_custom1"><b>Lorem</b></p>' . "\n";
+	echo '<h2 id="h2_custom2"><a href="index.php" class="link">Lorem ipsum</a></h2>' . "\n";
+	echo '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>' . "\n";
+	echo '</div>' . "\n"; 
+	echo "\n";
 	$count += 1;
 }
 ?>

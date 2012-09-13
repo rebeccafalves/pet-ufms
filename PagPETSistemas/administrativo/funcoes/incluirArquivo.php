28d9a7bef7ed2d3d32c011784d72fec818d9a930
Rebecca França Alves <rebeccafalves@gmail.com>
2012-09-13 15:52:21 -0400

    new file:   PagPETSistemas/administrativo/funcoes/incluirArquivo.php 	modified:   PagPETSistemas/administrativo/funcoes/incluirUsuario.php

---
+++ PagPETSistemas/administrativo/funcoes/incluirArquivo.php
@@ -0,0 +1,34 @@
+<?php
+
+function incluirArquivo($pasta) {
+    include ("../../includes/funcoes/conexao.php");
+    $foto = $_POST["file"];
+
+    $type = $_FILES['file']['type'];
+    $size = $_FILES['file']['size'];
+    if ($_FILES["file"]["error"] > 0) {
+        return 3; //erro
+    } else if ($type == "video/avi" || $size > 2000000) { //imagens que pode ser upload, e tamanho de arquivo maximo
+        return 4; //acima do limite
+    } else {
+        $temp = $_FILES['file']['tmp_name'];
+        $name = $_FILES['file']['name'];
+
+        move_uploaded_file($temp, "../../arquivos/" . $pasta . "/" . $name);
+
+        $query = <<<hereDoc
+insert into `pet-sistemas`.`arquivos` (caminho) values ('arquivos/$pasta/$name')
+hereDoc;
+    }
+    
+    $sql = mysql_query($query);
+
+
+    if ($sql == false) {
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+?>
--- PagPETSistemas/administrativo/funcoes/incluirUsuario.php
+++ PagPETSistemas/administrativo/funcoes/incluirUsuario.php
@@ -1,6 +1,6 @@
 <?php
 include ("../../includes/funcoes/conexao.php"); //arquivo de coneccao com o banco
-
+include ("/incluirArquivo.php"); //arquivo de coneccao com o banco
 //Obtencao das variaveis
 $nome = $_POST["nome"];
 $email = $_POST["email"];
@@ -32,64 +32,90 @@ $query = "SELECT usuario.id_usuario FROM usuario where usuario.nome_usuario = '$
 $query = mysql_query($query, $conexao);
 
 if (strlen($usuario) == 0) { // verifica se o nome passado na tela tem tamanho 0
-    $in = 3;
-	
+    $in = 5;
 } else if (mysql_fetch_array($query)) { // verifica se existe resultado na consulta
     echo "<script type='text/javascript'> alert('Já existe alguém com este nome de usuário!')</script>";
-    $in = 3;
-	
+    $in = 6;
 } else {
-    if ($radio == 'outro') { 
-		//Insere dados básicos da Pessoa
-        $ins = "INSERT INTO `pet-sistemas`.`usuario` (nome_usuario, senha, nome, permissao, descricao, email, status, lattes) VALUES ('$usuario', '$senha', '$nome', '$permissao', '$descricao', '$email', '$status', '$lattes')";
-        $insere = mysql_query($ins);
-    
-	} else if ($radio == 'tutor') {
-		//Insere dados básicos da Pessoa
-        $ins = "INSERT INTO `pet-sistemas`.`usuario` (nome_usuario, senha, nome, permissao, descricao, email, status, lattes) VALUES ('$usuario', '$senha', '$nome', '$permissao', '$descricao', '$email', '$status', '$lattes')";
-        $insere = mysql_query($ins);
-		
-        if ($insere) { //Caso a insercao ocorra o id do usuario inserido eh localizado
-            $query = "SELECT usuario.id_usuario FROM usuario where usuario.nome_usuario = '$usuario'";
-            $query = mysql_query($query, $conexao);
 
-			//Dados especificos de tutor sao inseridos usando o ID localizado
-            while ($dados = mysql_fetch_array($query)) {
-                $ins = "INSERT INTO `pet-sistemas`.`tutor` (data_ini, id_usuario) VALUES ('$data_ini_tutor', '$dados[0]')";
+    if ($radio == 'outro') {
+
+        $ret = incluirArquivo('usuarios'); // incluir o arquivo na pasta 'usuarios'
+
+        if ($ret == 1) { // Se o arquivo foi inserido
+            $name = $_FILES['file']['name']; //obtem o nome do arquivo
+
+            $query = "SELECT id_arquivos FROM arquivos WHERE caminho='arquivos/usuarios/$name order by id_arquivos desc'";
+            $res = mysql_query($query);
+
+            if ($dados = mysql_fetch_array($res)) {
+                $ins = "INSERT INTO `pet-sistemas`.`usuario` (nome_usuario, senha, nome, permissao, descricao, email, status, lattes, foto_id_arquivos) VALUES ('$usuario', '$senha', '$nome', '$permissao', '$descricao', '$email', '$status', '$lattes', '$dados[0]')";
                 $insere = mysql_query($ins);
             }
         }
-		
-    } else if ($radio == 'petiano') {
-		//Insere dados básicos da Pessoa
-        $ins = "INSERT INTO `pet-sistemas`.`usuario` (nome_usuario, senha, nome, permissao, descricao, email, status, lattes) VALUES ('$usuario', '$senha', '$nome', '$permissao', '$descricao', '$email', '$status', '$lattes')";
-        $insere = mysql_query($ins);
-        
-		if ($insere) {//Caso a insercao ocorra o id do usuario inserido eh localizado
-            $query = "SELECT usuario.id_usuario FROM usuario where usuario.nome_usuario = '$usuario'";
-            $query = mysql_query($query, $conexao);
-            
-			//Dados especificos do petiano sao inseridos usando o ID localizado
-			while ($dados = mysql_fetch_array($query)) {
-                $ins = "INSERT INTO `pet-sistemas`.`petiano` (rga, cpf, rg, org_exp, telefone, endereco, data_nasc, data_ini, id_usuario, pai, mae) VALUES ('$rga','$cpf','$rg','$orgao','$telefone','$endereco','$data_nasc','$data_ini_petiano','$dados[0]','$pai','$mae')";
+    } else if ($radio == 'tutor') {
+
+        $ret = incluirArquivo('usuarios'); // incluir o arquivo na pasta 'usuarios'
+        if ($ret == 1) { // Se o arquivo foi inserido
+            $name = $_FILES['file']['name']; //obtem o nome do arquivo
+
+            $query = "SELECT id_arquivos FROM arquivos WHERE caminho='arquivos/usuarios/$name order by id_arquivos desc'";
+            $res = mysql_query($query);
+
+            if ($dados = mysql_fetch_array($res)) {
+                //Insere dados básicos da Pessoa
+                $ins = "INSERT INTO `pet-sistemas`.`usuario` (nome_usuario, senha, nome, permissao, descricao, email, status, lattes, foto_id_arquivos) VALUES ('$usuario', '$senha', '$nome', '$permissao', '$descricao', '$email', '$status', '$lattes', '$dados[0]')";
                 $insere = mysql_query($ins);
+            }
+
+            if ($insere) { //Caso a insercao ocorra o id do usuario inserido eh localizado
+                $query = "SELECT usuario.id_usuario FROM usuario where usuario.nome_usuario = '$usuario'";
+                $query = mysql_query($query, $conexao);
+
+                //Dados especificos de tutor sao inseridos usando o ID localizado
+                while ($dados = mysql_fetch_array($query)) {
+                    $ins = "INSERT INTO `pet-sistemas`.`tutor` (data_ini, id_usuario) VALUES ('$data_ini_tutor', '$dados[0]')";
+                    $insere = mysql_query($ins);
+                }
+            }
+        }
+    } else if ($radio == 'petiano') {
+        $ret = incluirArquivo('usuarios'); // incluir o arquivo na pasta 'usuarios'
+        if ($ret == 1) { // Se o arquivo foi inserido
+            $name = $_FILES['file']['name']; //obtem o nome do arquivo
+
+            $query = "SELECT id_arquivos FROM arquivos WHERE caminho='arquivos/usuarios/$name order by id_arquivos desc'";
+            $res = mysql_query($query);
+
+            if ($dados = mysql_fetch_array($res)) {
+                //Insere dados básicos da Pessoa
+                $ins = "INSERT INTO `pet-sistemas`.`usuario` (nome_usuario, senha, nome, permissao, descricao, email, status, lattes, foto_id_arquivos) VALUES ('$usuario', '$senha', '$nome', '$permissao', '$descricao', '$email', '$status', '$lattes', '$dados[0]')";
+                $insere = mysql_query($ins);
+            }
+            if ($insere) {//Caso a insercao ocorra o id do usuario inserido eh localizado
+                $query = "SELECT usuario.id_usuario FROM usuario where usuario.nome_usuario = '$usuario'";
+                $query = mysql_query($query, $conexao);
+
+                //Dados especificos do petiano sao inseridos usando o ID localizado
+                while ($dados = mysql_fetch_array($query)) {
+                    $ins = "INSERT INTO `pet-sistemas`.`petiano` (rga, cpf, rg, org_exp, telefone, endereco, data_nasc, data_ini, id_usuario, pai, mae) VALUES ('$rga','$cpf','$rg','$orgao','$telefone','$endereco','$data_nasc','$data_ini_petiano','$dados[0]','$pai','$mae')";
+                    $insere = mysql_query($ins);
+                }
             }
         }
     }
     mysql_close($conexao); // coneccao finalizada
-
-	
-	//Define o retorno para a tela conforme o sucesso da ultima insercao
+    // Define o retorno para a tela conforme o sucesso da ultima insercao
     if ($insere == false)
         $in = 2;
-
     else
         $in = 1;
 }
-header("Location: ../index.php?cod=cadastrarUsuario&in=$in"); //encaminha para a tela de cadastro, passando o codigo de retorno
+header("Location: ../index.php?cod=cadastrarUsuarios&in=$in"); //encaminha para a tela de cadastro, passando o codigo de retorno
 ?>
 <script type="text/javascript">
 
     document.write(txt.length);
 
 </script>
+

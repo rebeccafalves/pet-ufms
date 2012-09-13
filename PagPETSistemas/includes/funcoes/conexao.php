608c702075e1b9510dba35b11f738331b8922b63
Rebecca França Alves <rebeccafalves@gmail.com>
2012-09-13 16:02:33 -0400

    modificacoes da rebecca

--- PagPETSistemas/includes/funcoes/conexao.php
+++ PagPETSistemas/includes/funcoes/conexao.php
@@ -1,4 +1,4 @@
 <?php
 $conexao = mysql_connect("localhost", "root", "root") or die("falhou ao acessar o bd");
-mysql_select_db("petsistemas", $conexao) or die("falhou ao acessar a tabela");
+mysql_select_db("pet-sistemas", $conexao) or die("falhou ao acessar a tabela");
 ?>

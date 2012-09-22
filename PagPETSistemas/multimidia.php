9e28f7df4fc8fc68c68467877b1dbcbcbdd81779
Rebecca França Alves <rebeccafalves@gmail.com>
2012-09-21 21:35:51 -0400

    new file:   PagPETSistemas/arquivos/multimidia/20120824_193145.jpg 	new file:   PagPETSistemas/arquivos/multimidia/20120824_193213.jpg 	new file:   PagPETSistemas/arquivos/multimidia/20120824_193217.jpg 	new file:   PagPETSistemas/arquivos/noticias/ufms.jpg 	new file:   PagPETSistemas/arquivos/projetos/csu.gif 	new file:   PagPETSistemas/arquivos/publicacoes/pub.jpg 	new file:   PagPETSistemas/arquivos/usuarios/315305_394451153943454_1765037073_n.jpg 	new file:   PagPETSistemas/arquivos/usuarios/427203_394451237276779_319295667_n.jpg 	new file:   PagPETSistemas/arquivos/usuarios/430233_394451323943437_1318620508_n_copy.jpg 	new file:   PagPETSistemas/arquivos/usuarios/Chrysanthemum.jpg 	new file:   PagPETSistemas/arquivos/usuarios/Desert.jpg 	new file:   PagPETSistemas/arquivos/usuarios/Hydrangeas.jpg 	new file:   PagPETSistemas/arquivos/usuarios/Penguins.jpg 	new file:   PagPETSistemas/arquivos/usuarios/bla.jpg 	new file:   PagPETSistemas/arquivos/usuarios/blabla.jpg 	new file:   PagPETSistemas/arquivos/usuarios/lalala.jpg 	modified:   PagPETSistemas/multimidia.php 	modified:   pet-sistemas.sql 	modified:   petsistemas-class.asta 	new file:   petsistemas-class.asta.bak

--- PagPETSistemas/multimidia.php
+++ PagPETSistemas/multimidia.php
@@ -1,1 +1,40 @@
 <h3>Multimídia</h3>
+<table>
+    <tr>
+        <td>
+            <?php
+            //Consulta e exibicao dos dados de tutor
+            include ("includes/funcoes/conexao.php");
+            $query = "select id_arquivos, titulo, data from multimidia";
+            $result = mysql_query($query);
+            $num = mysql_num_rows($result);
+            if ($num == 0) {
+                echo "Não há Imagens.";
+            }
+            for ($i = 0; $i < $num; $i++) {
+                $row = mysql_fetch_row($result);
+                $query2 = "select caminho from arquivos where id_arquivos =$row[0]";
+                $result2 = mysql_query($query2);
+                $num2 = mysql_num_rows($result2);
+                for ($j = 0; $j < $num2; $j++) {
+                    ?>
+                    <b><h3>
+                            <?php echo $row[1]; ?> 
+                        </h3></b>
+                    <b> <?php
+                    echo "Em: " . substr($row[2], 8, 2) . "/" . substr($row[2], 5, 2) . "/" . substr($row[2], 0, 4);
+                    $img = mysql_fetch_row($result2);
+                            ?>
+                    </b>
+                    <img src="<?php echo $img[0] ?>" alt="A imagem não foi encontrada" width="800" height="800" style="float:right" ></img><br/>
+                    <?php
+                    echo "<br/>";
+                }
+                echo "<br/>";
+            }
+
+            echo "<br/><br/><br/>";
+            ?>
+        </td>
+    </tr>
+</table>    
--- pet-sistemas.sql
+++ pet-sistemas.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 06/09/2012 às 18h36min
--- Versão do Servidor: 5.5.25
+-- Tempo de Geração: 21/09/2012 às 22h25min
+-- Versão do Servidor: 5.5.22
 -- Versão do PHP: 5.3.9
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
@@ -31,7 +31,30 @@ CREATE TABLE IF NOT EXISTS `arquivos` (
   `caminho` varchar(100) NOT NULL,
   `data` date NOT NULL,
   PRIMARY KEY (`id_arquivos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+
+--
+-- Extraindo dados da tabela `arquivos`
+--
+
+INSERT INTO `arquivos` (`id_arquivos`, `caminho`, `data`) VALUES
+(1, 'arquivos/usuarios/Chrysanthemum.jpg', '2012-09-14'),
+(2, 'arquivos/usuarios/Desert.jpg', '2012-09-14'),
+(3, 'arquivos/usuarios/Hydrangeas.jpg', '2012-09-14'),
+(4, 'arquivos/usuarios/Penguins.jpg', '2012-09-14'),
+(5, 'arquivos/usuarios/315305_394451153943454_1765037073_n.jpg', '2012-09-14'),
+(6, 'arquivos/usuarios/427203_394451237276779_319295667_n.jpg', '2012-09-14'),
+(7, 'arquivos/usuarios/430233_394451323943437_1318620508_n_copy.jpg', '2012-09-14'),
+(8, 'arquivos/usuarios/blabla.jpg', '2012-09-14'),
+(9, 'arquivos/usuarios/lalala.jpg', '2012-09-17'),
+(10, 'arquivos/usuarios/bla.jpg', '2012-09-18'),
+(11, 'arquivos/multimidia/20120824_193145.jpg', '2012-09-20'),
+(12, 'arquivos/multimidia/20120824_193213.jpg', '2012-09-20'),
+(13, 'arquivos/multimidia/20120824_193217.jpg', '2012-09-20'),
+(14, 'arquivos/noticias/ufms.jpg', '2012-09-21'),
+(15, 'arquivos/projetos/csu.gif', '2012-09-21'),
+(16, 'arquivos/publicacoes/pub.jpg', '2012-09-21'),
+(17, 'arquivos/faca_parte/pdf.pdf', '2012-09-21');
 
 -- --------------------------------------------------------
 
@@ -59,7 +82,38 @@ CREATE TABLE IF NOT EXISTS `faca_parte` (
   `id_faca_parte` int(10) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id_faca_parte`),
   KEY `caminho_id_arquivos` (`caminho_id_arquivos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `faca_parte`
+--
+
+INSERT INTO `faca_parte` (`caminho_id_arquivos`, `id_faca_parte`) VALUES
+(17, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `multimidia`
+--
+
+CREATE TABLE IF NOT EXISTS `multimidia` (
+  `id_multimidia` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_arquivos` int(10) unsigned NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`id_multimidia`),
+  KEY `arquivo` (`id_arquivos`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `multimidia`
+--
+
+INSERT INTO `multimidia` (`id_multimidia`, `id_arquivos`, `titulo`, `data`) VALUES
+(1, 11, 'Reunião do Pet 1', '2012-08-24'),
+(2, 12, 'Reunião do Pet 2', '2012-08-24'),
+(3, 13, 'Reunião do Pet 3', '2012-08-24');
 
 -- --------------------------------------------------------
 
@@ -79,7 +133,14 @@ CREATE TABLE IF NOT EXISTS `noticia` (
   PRIMARY KEY (`id_noticia`),
   KEY `autor` (`autor`),
   KEY `caminho_id_arquivos` (`caminho_id_arquivos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `noticia`
+--
+
+INSERT INTO `noticia` (`id_noticia`, `data`, `titulo`, `conteudo`, `autor`, `assunto`, `tipo`, `caminho_id_arquivos`) VALUES
+(1, '2012-01-20', 'UFMS de Portas Abertas', 'A UFMS está realizando durante esta semana o evento UFMS de portas abertas onde os estudantes estão apresentando seus projetos.', 2, '', '3', 14);
 
 -- --------------------------------------------------------
 
@@ -100,13 +161,19 @@ CREATE TABLE IF NOT EXISTS `petiano` (
   `id_usuario` int(11) unsigned NOT NULL,
   `pai` varchar(50) NOT NULL,
   `mae` varchar(50) NOT NULL,
-  `foto_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`rga`),
   UNIQUE KEY `cpf` (`cpf`),
   UNIQUE KEY `rg` (`rg`,`org_exp`),
-  KEY `id_usuario` (`id_usuario`),
-  KEY `foto_id` (`foto_id`)
+  KEY `id_usuario` (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `petiano`
+--
+
+INSERT INTO `petiano` (`rga`, `cpf`, `rg`, `org_exp`, `telefone`, `endereco`, `data_nasc`, `data_ini`, `data_fim`, `id_usuario`, `pai`, `mae`) VALUES
+('0000111', '09991919', '9191991', 'SSP', '919191', 'Casa', '1990-10-10', '1990-10-10', NULL, 10, 'Casa', 'Casa'),
+('1010101', '101010', '10101010101', 'ssp', '101010', 'Teste Petiano', '1990-10-10', '2011-10-10', NULL, 2, 'Teste Petiano', 'Teste Petiano');
 
 -- --------------------------------------------------------
 
@@ -126,7 +193,14 @@ CREATE TABLE IF NOT EXISTS `projeto` (
   PRIMARY KEY (`id_projeto`),
   KEY `arquivo_id_arquivos` (`arquivo_id_arquivos`),
   KEY `projeto_prop` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `projeto`
+--
+
+INSERT INTO `projeto` (`nome`, `data_ini`, `data_fim`, `descricao`, `tipo`, `id_projeto`, `arquivo_id_arquivos`, `id_usuario`) VALUES
+('CSU', '2012-04-10', '2012-09-20', 'Projeto desenvolvido pelo pet. Computação sem Computador', 3, 1, 15, 4);
 
 -- --------------------------------------------------------
 
@@ -135,15 +209,21 @@ CREATE TABLE IF NOT EXISTS `projeto` (
 --
 
 CREATE TABLE IF NOT EXISTS `publicacoes` (
-  `autor` varchar(100) NOT NULL,
   `titulo` varchar(100) NOT NULL,
   `assunto` varchar(100) NOT NULL,
   `caminho_id_arquivos` int(10) unsigned NOT NULL,
   `data_pub` date NOT NULL,
-  `id_publicacoes` int(11) NOT NULL AUTO_INCREMENT,
+  `id_publicacoes` int(11) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id_publicacoes`),
   KEY `caminho_id_arquivos` (`caminho_id_arquivos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `publicacoes`
+--
+
+INSERT INTO `publicacoes` (`titulo`, `assunto`, `caminho_id_arquivos`, `data_pub`, `id_publicacoes`) VALUES
+('Primeira Publicação', 'Grafos', 16, '2012-04-13', 1);
 
 -- --------------------------------------------------------
 
@@ -158,7 +238,17 @@ CREATE TABLE IF NOT EXISTS `tutor` (
   `id_usuario` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id_tutor`),
   KEY `id_usuario` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Extraindo dados da tabela `tutor`
+--
+
+INSERT INTO `tutor` (`data_ini`, `data_fim`, `id_tutor`, `id_usuario`) VALUES
+('2010-10-10', NULL, 1, 1),
+('2000-10-12', NULL, 2, 3),
+('2000-10-13', NULL, 3, 4),
+('2000-12-12', NULL, 4, 6);
 
 -- --------------------------------------------------------
 
@@ -181,7 +271,23 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   UNIQUE KEY `nome_usuario_UNIQUE` (`nome_usuario`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `foto_id_arquivos` (`foto_id_arquivos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `senha`, `nome`, `permissao`, `descricao`, `email`, `foto_id_arquivos`, `status`, `lattes`) VALUES
+(1, 'Teste Tutor', 'ba0b5032731e090f7806d91e2ad4a843', 'Teste Tutor', 1, 'Teste Tutor', 'Teste Tutor', 1, 'A', 'Teste Tutor'),
+(2, 'Teste Petiano', '18b0270f6d1871acdf65c2f3d3f59f04', 'Teste Petiano', 1, 'Teste Petiano', 'Teste Petiano', 2, 'A', 'Teste Petiano'),
+(3, 'Testes', 'd7d22c0d9dc724ff9bc2ae570b54012a', 'Testes', 1, 'Testes', 'Testes', 3, 'A', 'Testes'),
+(4, 'Mais Teste ', '49d879685b37089724d8c9010ddd7811', 'Mais Teste ', 1, 'Mais Teste ', 'Mais Teste ', 4, 'A', 'Mais Teste '),
+(5, 'aaa', '47bce5c74f589f4867dbd57e9ca9f808', 'aaa', 1, 'aaa', 'aaa', 5, 'A', 'aaa'),
+(6, 'bbb', '08f8e0260c64418510cefb2b06eee5cd', 'bbb', 1, 'bbb', 'bbb', 6, 'A', 'bbb'),
+(7, 'Teste', '8e6f6f815b50f474cf0dc22d4f400725', 'Teste', 1, 'Teste', 'Teste', 7, 'A', 'Teste'),
+(8, 'Bla', '756eae833e2a14bc716861be073dd4b8', 'Bla', 1, 'Bla', 'Bla', 8, 'A', 'Bla'),
+(9, 'lalala', '9aa6e5f2256c17d2d430b100032b997c', 'lalala', 1, 'lalala', 'lalala', 9, 'A', 'lalala'),
+(10, 'Casa', 'ca90fdc7b90b2152e5dcccefb2c828f7', 'Casa', 1, 'Casa', 'Casa', 10, 'A', 'Casa');
 
 -- --------------------------------------------------------
 
@@ -197,6 +303,38 @@ CREATE TABLE IF NOT EXISTS `usuario_projeto` (
   KEY `id_projeto` (`id_projeto`),
   KEY `id_usuario` (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuario_projeto`
+--
+
+INSERT INTO `usuario_projeto` (`id_usuario`, `id_projeto`, `data_ini`) VALUES
+(2, 1, '2012-04-10');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario_publicacoes`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario_publicacoes` (
+  `id_usuario_publicacoes` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(10) unsigned NOT NULL,
+  `id_publicacoes` int(10) unsigned NOT NULL,
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`id_usuario_publicacoes`),
+  UNIQUE KEY `id_usuario_UNIQUE` (`id_usuario`),
+  UNIQUE KEY `id_publicacoes_UNIQUE` (`id_publicacoes`),
+  KEY `usuario` (`id_usuario`),
+  KEY `publicacao` (`id_publicacoes`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `usuario_publicacoes`
+--
+
+INSERT INTO `usuario_publicacoes` (`id_usuario_publicacoes`, `id_usuario`, `id_publicacoes`, `data`) VALUES
+(1, 2, 1, '2012-04-13');
 
 --
 -- Restrições para as tabelas dumpadas
@@ -215,6 +353,12 @@ ALTER TABLE `faca_parte`
   ADD CONSTRAINT `faca_parte_ibfk_1` FOREIGN KEY (`caminho_id_arquivos`) REFERENCES `arquivos` (`id_arquivos`);
 
 --
+-- Restrições para a tabela `multimidia`
+--
+ALTER TABLE `multimidia`
+  ADD CONSTRAINT `arquivo` FOREIGN KEY (`id_arquivos`) REFERENCES `arquivos` (`id_arquivos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Restrições para a tabela `noticia`
 --
 ALTER TABLE `noticia`
@@ -225,7 +369,6 @@ ALTER TABLE `noticia`
 -- Restrições para a tabela `petiano`
 --
 ALTER TABLE `petiano`
-  ADD CONSTRAINT `petiano_ibfk_3` FOREIGN KEY (`foto_id`) REFERENCES `arquivos` (`id_arquivos`),
   ADD CONSTRAINT `petiano_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
 --
@@ -259,6 +402,13 @@ ALTER TABLE `usuario`
 ALTER TABLE `usuario_projeto`
   ADD CONSTRAINT `usuario_projeto_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
   ADD CONSTRAINT `usuario_projeto_ibfk_2` FOREIGN KEY (`id_projeto`) REFERENCES `projeto` (`id_projeto`);
+
+--
+-- Restrições para a tabela `usuario_publicacoes`
+--
+ALTER TABLE `usuario_publicacoes`
+  ADD CONSTRAINT `publicacao` FOREIGN KEY (`id_publicacoes`) REFERENCES `publicacoes` (`id_publicacoes`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

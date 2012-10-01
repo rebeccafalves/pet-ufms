6058650747a64fc7bcf25a33bd62019b60784ac8
TamaraMendes <ahliv.tamara@gmail.com>
2012-09-30 22:38:14 -0400

    modified:   PagPETSistemas/noticias.php 	modified:   PagPETSistemas/style.css

--- PagPETSistemas/noticias.php
+++ PagPETSistemas/noticias.php
@@ -1,4 +1,5 @@
 ﻿<h1>Notícias</h1>
+
 <div style =	"border-top: 1px solid gray;
 				border-bottom: 1px solid gray;
 				margin-bottom: 50px;
@@ -7,6 +8,7 @@
 				
 <h2>Últimas notícias<h2>
 </div>
+
 <?php //Consulta e exibicao dos dados de tutor
     include ("includes/funcoes/conexao.php");
     $query = "select n.titulo, n.tipo, n.conteudo, n.caminho_id_arquivos from noticia n";
@@ -24,23 +26,24 @@
     
 
         ?>
-        <img src= <?php echo $linha[0]; ?> alt="A imagem não foi encontrada" width="300" height="300" > </img>
+        <div class='imagemnoticia'>
+            <img src='<?php echo $linha[0]; ?>' " width="76px" height="76px"  alt="A imagem nao foi encontrada"  >
+        </div>
         <?php 
 
-
         echo "<div class='noticia'> <b>Titulo:</b> $row[0]<br/>";
-        echo "<b>tipo:</b>";
-        if($row[1] == "1")
-            echo "ensino <br/>";
-        else if($row[1] == "2")
-            echo "pesquisa <br/>";
-        else
-            echo "extens&atildeo <br/>";
-        
-        echo "<b>conteudo:</b> $row[2]<br/>";
-        echo "<br/> <hr>";
+            echo "<b>tipo:</b>";
+            if($row[1] == "1")
+                echo "ensino <br/>";
+            else if($row[1] == "2")
+                echo "pesquisa <br/>";
+            else
+                echo "extens&atildeo <br/>";
+            echo "<b>conteudo:</b> $row[2]<br/>";
+            echo "<br/>";
         echo "</div>";
-        //pelo amor de Deus de certooooooo
+        echo "<hr>";
+        //pelo amor de Deus de certooooooo!
     }
     
 ?>
